a309f07f99d2c2c8a04747d09164c50a688cdd75
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-08-22 02:17:45 +0500

    react-first-ass

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+
+createRoot(document.getElementById('root')).render(
+  
+
+  <div>
+      <ul>
+        <li><App/></li>
+      </ul>
+  </div>
+)
